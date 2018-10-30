0705ab10fd5a1393f084ea6375137b14122b0946
monkog <monkog@wp.pl>
2018-10-31 00:11:51 +0100

    Command and math unit tests

--- BrownianMotion/Helpers/DelegateCommand.cs
+++ BrownianMotion/Helpers/DelegateCommand.cs
@@ -6,6 +6,7 @@ namespace BrownianMotion.Helpers
     public class DelegateCommand : ICommand
     {
         private readonly Predicate<object> _canExecute;
+
         private readonly Action<object> _execute;
 
         public event EventHandler CanExecuteChanged;
@@ -21,12 +22,12 @@ namespace BrownianMotion.Helpers
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object parameter = null)
         {
-            return (_canExecute == null) || _canExecute(parameter);
+            return _canExecute == null || _canExecute(parameter);
         }
 
-        public void Execute(object parameter)
+        public void Execute(object parameter = null)
         {
             _execute(parameter);
         }
---
+++ BrownianMotionTests/Helpers/NumberCollectionExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using BrownianMotion.Helpers;
+using Xunit;
+
+namespace BrownianMotionTests.Helpers
+{
+	public class NumberCollectionExtensionsTests
+	{
+		[Fact]
+		public void CumulatedSum_Collection_SumArray()
+		{
+			var array = new[] {1.0, 2.0, 3.0, 4.0};
+			var expected = new[] { 1.0, 3.0, 6.0, 10.0 };
+
+			var sum = array.CumulatedSum();
+
+			Assert.Equal(expected, sum);
+		}
+	}
+}
